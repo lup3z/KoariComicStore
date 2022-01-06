d32689a85e9a74ca156db205399a233b17df0403
Lucia <lucia.aznar1987@gmail.com>
2022-01-06 12:37:39 -0300

    Se agrega base prod

---
+++ src/controllers/apiUsers.js
@@ -0,0 +1,18 @@
+//const { model } = require('../models/usersModel');
+
+let apiUsers = {
+
+    /* GET: All Users API */
+    //allUsers: async function (req, res) {
+        
+       // try {
+       //     const result = await model.findAll();
+       //     res.status(200).json({data: result, error: null, succes: true})
+       // } catch (error) {
+       //     res.status(500).json({data: null, error: error, succes: false})
+        //}
+    //},
+
+}
+
+module.exports = apiUsers
