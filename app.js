7bbc301bdde853646641ab9022ebfe969e49519d
Lucia <lucia.aznar1987@gmail.com>
2022-01-16 12:28:34 -0300

    subo logica de react 2da parte

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const userLoggedMiddleware = require('./src/middlewares/userLoggedMiddleware');
 const path = require("path");
 const methodOverride = require('method-override');
 const app = express();
+const cors = require("cors")
 const puerto = process.env.PORT || 3030
 
 app.set("views", path.join(__dirname, "./src/views"));
 app.set("view engine", "ejs");
+app.use(cors());
 
 app.use(session({
   secret: "Shhh, It's a secret",
@@ -24,7 +26,8 @@ app.use(express.json());
 
 app.use(methodOverride("_method"));
 app.use("/", require("./src/routes/index.routes"));
-app.use("/api", require("./src/routes/index.routes"));
+app.use("/api", cors(), require("./src/routes/index.routes"));
+
 app.use((req, res, next) => {
   res.status(404).render('not-found')
 });
