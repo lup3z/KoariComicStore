9bb9b0931be8c4dfaf0d855d2ee79b07895df19d
Lucia <lucia.aznar1987@gmail.com>
2022-01-16 12:26:45 -0300

    subo logica de react

---
+++ dashboard/src/components/TotalUsuarios.js
@@ -0,0 +1,46 @@
+import React, {Component} from "react";
+
+class TotalUsuarios extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            totalUsus: [],
+            url: "https://koaricomic.herokuapp.com/api/users"
+        }
+    }
+
+    apiCall(url, consecuencia){
+        fetch(url)
+            .then(response => response.json())
+            .then (data => consecuencia(data))
+            .catch(error => console.log(error))
+    }
+
+    componentDidMount() {
+        console.log("-----")
+        this.apiCall(this.state.url, this.mostrarInfo )
+        
+      }
+
+      mostrarInfo = (data) => {
+          console.log(data)
+          this.setState(
+              {
+                totalUsus: data.count
+              }
+          )
+      }
+      render() {
+        
+        return (
+          <div>
+              <p>La cantidad de usuarios de la tienda es: {this.state.totalUsus}</p>
+          </div>
+        );
+        
+    }
+
+}
+
+export default TotalUsuarios;
---
+++ dashboard/src/components/UltimoProductoCreado.js
@@ -0,0 +1,44 @@
+import React, {Component} from "react";
+
+class UltimoProductoCreado extends Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            ultimoProducto: [],
+            url: "https://koaricomic.herokuapp.com/api/products"
+        }
+    }
+
+    apiCall(url, consecuencia){
+        fetch(url)
+            .then(response => response.json())
+            .then (data => consecuencia(data))
+            .catch(error => console.log(error))
+    }
+
+    componentDidMount() {
+        console.log("-----")
+        this.apiCall(this.state.url, this.mostrarInfo )
+        
+      }
+
+      mostrarInfo = (data) => {
+          console.log(data)
+          this.setState({
+            ultimoProducto: data.products[data.products.length -1].name
+              })
+      }
+      render() {
+        
+        return (
+          <div>
+              <p>El ultimo elemento creado en la tienda es: {this.state.ultimoProducto}</p>
+          </div>
+        );
+        
+    }
+
+}
+
+export default UltimoProductoCreado;
