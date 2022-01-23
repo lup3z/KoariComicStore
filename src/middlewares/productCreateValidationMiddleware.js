1f3e6a26d0e26f8eb032123afd88421b6e10b066
nmgomezok <nmgomezok@gmail.com>
2022-01-23 16:57:14 -0300

    minnor fix

--- src/middlewares/productCreateValidationMiddleware.js
+++ src/middlewares/productCreateValidationMiddleware.js
@@ -3,43 +3,43 @@ const path = require('path');
 
 const productCreateValidationMiddleware = [
     body('nombre')
-		.notEmpty().withMessage('Tienes que escribir un nombre para el producto').bail()
+		.notEmpty().withMessage('Tienes que detallar un nombre para el producto').bail()
 		.isLength({ min: 5 }).withMessage('El nombre del producto debe ser más largo'),
 
     body('descripcion')
-		.notEmpty().withMessage('Tienes que escribir una descripción').bail()
+		.notEmpty().withMessage('Tienes que incorporar una descripción del producto').bail()
 		.isLength({ min: 8 }).withMessage('La descripción debe ser más larga'),
 
 	body('volumen')
-		.notEmpty().withMessage('Tienes que escribir a qué volumen pertenece').bail()
+		.notEmpty().withMessage('Tienes que detallar a qué volumen pertenece').bail()
 		.isLength({ min: 3 }).withMessage('El volumen debe ser más largo'),
     
 	body('autor')
-		.notEmpty().withMessage('Tienes que escribir quién es el autor').bail()
+		.notEmpty().withMessage('Tienes que detallar el nombre del autor').bail()
 		.isLength({ min: 8 }).withMessage('El nombre del autor debe ser más largo'),
 
 	body('artista')
-		.notEmpty().withMessage('Tienes que escribir el nombre del artista').bail()
+		.notEmpty().withMessage('Tienes que detallar el nombre del artista').bail()
 		.isLength({ min: 8 }).withMessage('El nombre del artista debe ser más largo'),
 
     body('editorial')
-		.notEmpty().withMessage('Tienes que escribir el nombre de la editorial').bail()
+		.notEmpty().withMessage('Tienes que detallar el nombre de la editorial').bail()
 		.isLength({ min: 5 }).withMessage('El nombre de la editorial debe ser más largo'),
 
     body('qDePaginas')
-		.notEmpty().withMessage('Tienes que escribir la cantidad de páginas').bail()
+		.notEmpty().withMessage('Tienes que indicar la cantidad de páginas').bail()
 		.isNumeric(),
 
     body('colorObyn')
-		.notEmpty().withMessage('Tienes que escribir si el producto es a color o blanco & negro').bail()
+		.notEmpty().withMessage('Tienes que indicar si el producto es a color o blanco & negro').bail()
 		.isLength({ min: 4 }).withMessage('La información sobre la tinta del producto debe ser más larga'),
 
     body('edicion')
-		.notEmpty().withMessage('Tienes que escribir el nombre de la edición').bail()
+		.notEmpty().withMessage('Tienes que detallar el nombre de la edición').bail()
 		.isLength({ min: 5 }).withMessage('El nombre de la edición debe ser más largo'),
 
     body('precio')
-		.notEmpty().withMessage('Tienes que escribir el precio').bail()
+		.notEmpty().withMessage('Tienes que indicar el precio').bail()
 		.isNumeric(),
 
     body('imagen').custom((value, { req }) => {
